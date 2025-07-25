078a812cc78d8dd8c431b6b8257b0d19bfc61864
Shonhh <endinja.versitile@gmail.com>
2025-07-24 20:04:59 -0500

    forgot to add hypr-panel/paper

---
+++ modules/desktop/hyprpanel.nix
@@ -0,0 +1,7 @@
+{ inputs, ... }:
+
+{
+  programs.hyprpanel = {
+    enabled = true
+  };
+}
---
+++ modules/desktop/hyprpaper.nix
@@ -0,0 +1,12 @@
+{
+  services.hyprpaper = {
+    enable = true;
+
+    settings = {
+      splash = true;
+
+      preload = [ "~/Images/wallpaper.png" ];
+      wallpaper = [ "~/Images/wallpaper.png" ];
+    };
+  };
+}
