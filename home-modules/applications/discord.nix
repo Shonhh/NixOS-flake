4c4504f816af44d83daf5e374b921b1e9547234f
= <endinja.versitile@gmail.com>
2025-08-13 22:09:38 -0500

    made zoom for eDP-1 an option

--- home-modules/applications/discord.nix
+++ home-modules/applications/discord.nix
@@ -1,83 +1,84 @@
-{ inputs, ... }: {
+{ inputs, ... }:
+{
   imports = [
     inputs.nixcord.homeModules.nixcord
   ];
 
   programs.nixcord = {
     enable = true;
-    
+
     quickCss = ./discord.css;
 
     config = {
       plugins = {
         anonymiseFileNames.enable = true;
-	betterGifAltText.enable = true;
-	betterGifPicker.enable = true;
-	betterRoleContext.enable = true;
-	betterRoleDot.enable = true;
-	betterSessions.enable = true;
-	betterSettings.enable = true;
-	betterUploadButton.enable = true;
-	biggerStreamPreview.enable = true;
-	blurNSFW.enable = true;
-	callTimer.enable = true;
-	clearURLs.enable = true;
+        betterGifAltText.enable = true;
+        betterGifPicker.enable = true;
+        betterRoleContext.enable = true;
+        betterRoleDot.enable = true;
+        betterSessions.enable = true;
+        betterSettings.enable = true;
+        betterUploadButton.enable = true;
+        biggerStreamPreview.enable = true;
+        blurNSFW.enable = true;
+        callTimer.enable = true;
+        clearURLs.enable = true;
 
-	clientTheme = {
-	  enable = true;
-	  color = "1e1e2e";
-	};
+        clientTheme = {
+          enable = true;
+          color = "1e1e2e";
+        };
 
-	copyFileContents.enable = true;
-	copyUserURLs.enable = true;
-	customRPC.enable = true;
-	dearrow.enable = true;
-	dontRoundMyTimestamps.enable = true;
-	fakeNitro.enable = true;
-	fixCodeblockGap.enable = true;
-	fixImagesQuality.enable = true;
-	fixSpotifyEmbeds.enable = true;
-	fixYoutubeEmbeds.enable = true;
-	friendsSince.enable = true;
-	fullSearchContext.enable = true;
-	gameActivityToggle.enable = true;
-	imageZoom.enable = true;
-	implicitRelationships.enable = true;
-	keepCurrentChannel.enable = true;
-	memberCount.enable = true;
-	mentionAvatars.enable = true;
-	messageClickActions.enable = true;
-	messageLinkEmbeds.enable = true;
-	messageLogger.enable = true;
-	noUnblockToJump.enable = true;
-	openInApp.enable = true;
-	permissionFreeWill.enable = true;
-	platformIndicators.enable = true;
-	relationshipNotifier.enable = true;
-	revealAllSpoilers.enable = true;
-	roleColorEverywhere.enable = true;
-	serverInfo.enable = true;
-	shikiCodeblocks.enable = true;
-	showHiddenChannels.enable = true;
-	showHiddenThings.enable = true;
-	
-	showMeYourName = {
-	  enable = true;
-	  mode = "nick-user";
-	};
+        copyFileContents.enable = true;
+        copyUserURLs.enable = true;
+        customRPC.enable = true;
+        dearrow.enable = true;
+        dontRoundMyTimestamps.enable = true;
+        fakeNitro.enable = true;
+        fixCodeblockGap.enable = true;
+        fixImagesQuality.enable = true;
+        fixSpotifyEmbeds.enable = true;
+        fixYoutubeEmbeds.enable = true;
+        friendsSince.enable = true;
+        fullSearchContext.enable = true;
+        gameActivityToggle.enable = true;
+        imageZoom.enable = true;
+        implicitRelationships.enable = true;
+        keepCurrentChannel.enable = true;
+        memberCount.enable = true;
+        mentionAvatars.enable = true;
+        messageClickActions.enable = true;
+        messageLinkEmbeds.enable = true;
+        messageLogger.enable = true;
+        noUnblockToJump.enable = true;
+        openInApp.enable = true;
+        permissionFreeWill.enable = true;
+        platformIndicators.enable = true;
+        relationshipNotifier.enable = true;
+        revealAllSpoilers.enable = true;
+        roleColorEverywhere.enable = true;
+        serverInfo.enable = true;
+        shikiCodeblocks.enable = true;
+        showHiddenChannels.enable = true;
+        showHiddenThings.enable = true;
 
-	showTimeoutDuration.enable = true;
-	sortFriendRequests.enable = true;
-	spotifyCrack.enable = true;
-	spotifyShareCommands.enable = true;
-	typingIndicator.enable = true;
-	typingTweaks.enable = true;
-	unlockedAvatarZoom.enable = true;
-	userVoiceShow.enable = true;
-	validReply.enable = true;
-	validUser.enable = true;
-	volumeBooster.enable = true;
-	youtubeAdblock.enable = true;
+        showMeYourName = {
+          enable = true;
+          mode = "nick-user";
+        };
+
+        showTimeoutDuration.enable = true;
+        sortFriendRequests.enable = true;
+        spotifyCrack.enable = true;
+        spotifyShareCommands.enable = true;
+        typingIndicator.enable = true;
+        typingTweaks.enable = true;
+        unlockedAvatarZoom.enable = true;
+        userVoiceShow.enable = true;
+        validReply.enable = true;
+        validUser.enable = true;
+        volumeBooster.enable = true;
+        youtubeAdblock.enable = true;
       };
     };
   };
--- home-modules/hypr/hyprland.nix
+++ home-modules/hypr/hyprland.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   config,
   pkgs,
   inputs,
@@ -6,265 +7,271 @@
 }:
 
 {
-  wayland.windowManager.hyprland = {
-    enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-  };
+  options.eDP-1.enable = lib.mkEnableOption "scale x2 on eDP-1";
 
-  wayland.windowManager.hyprland.settings = {
-    # Monitors
-    monitor = [
-      "Virtual-1,2048x1152@60.00,auto,1"
-      "eDP-1,highres@highrr,auto,2"
-      ",highres@highrr,auto,1"
-    ];
-
-    # XWayland
-    xwayland = {
-      force_zero_scaling = true;
+  config = {
+    wayland.windowManager.hyprland = {
+      enable = true;
+      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+      portalPackage =
+        inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     };
 
-    # Programs
-    "$terminal" = "ghostty";
-    "$browser" = "vivaldi";
-    "$file_manager" = "thunar";
-    "$code_editor" = "code";
+    wayland.windowManager.hyprland.settings = {
+      # Monitors
+      monitor = [
+        "Virtual-1,2048x1152@60.00,auto,1"
+      ]
+      ++ lib.optional config.eDP-1.enable "eDP-1,highres@highrr,auto,2"
+      ++ [
+        ",highres@highrr,auto,1"
+      ];
 
-    # Autostart
-    exec-once = [
-      "$terminal"
+      # XWayland
+      xwayland = {
+        force_zero_scaling = true;
+      };
 
-      # ... add more later
-    ];
+      # Programs
+      "$terminal" = "ghostty";
+      "$browser" = "vivaldi";
+      "$file_manager" = "thunar";
+      "$code_editor" = "code";
 
-    # Environment
-    env = [
-      "XCURSOR_SIZE,23"
-      "HYPRCURSOR_SIZE,23"
+      # Autostart
+      exec-once = [
+        "$terminal"
 
-      "XCURSOR_THEME,catppuccin-mocha-dark-cursors"
-      "HYPRCURSOR_THEME,catppuccin-mocha-dark-cursors"
-      "WLR_NO_HARDWARE_CURSORS,1"
-      "LIBVA_DRIVER_NAME,nvidia"
-      "__GLX_VENDOR_LIBRARY_NAME,nvidia"
-    ];
+        # ... add more later
+      ];
 
-    # General
-    general = {
-      gaps_in = 3;
-      gaps_out = 8;
+      # Environment
+      env = [
+        "XCURSOR_SIZE,23"
+        "HYPRCURSOR_SIZE,23"
 
-      border_size = 2;
+        "XCURSOR_THEME,catppuccin-mocha-dark-cursors"
+        "HYPRCURSOR_THEME,catppuccin-mocha-dark-cursors"
+        "WLR_NO_HARDWARE_CURSORS,1"
+        "LIBVA_DRIVER_NAME,nvidia"
+        "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+      ];
 
-      resize_on_border = true;
+      # General
+      general = {
+        gaps_in = 3;
+        gaps_out = 8;
 
-      allow_tearing = false;
-      layout = "dwindle";
-    };
+        border_size = 2;
 
-    # Decoration
-    decoration = {
-      rounding = 7;
-      active_opacity = 1.0;
-      inactive_opacity = 1.0;
+        resize_on_border = true;
 
-      blur = {
+        allow_tearing = false;
+        layout = "dwindle";
+      };
+
+      # Decoration
+      decoration = {
+        rounding = 7;
+        active_opacity = 1.0;
+        inactive_opacity = 1.0;
+
+        blur = {
+          enabled = true;
+          size = 5;
+          passes = 3;
+          new_optimizations = "on";
+          ignore_opacity = "on";
+          xray = false;
+        };
+      };
+
+      # Animations
+      animations = {
         enabled = true;
-        size = 5;
-        passes = 3;
-        new_optimizations = "on";
-        ignore_opacity = "on";
-        xray = false;
+
+        bezier = [
+          "wind, 0.05, 0.85, 0.03, 0.97"
+          "winIn, 0.07, 0.88, 0.04, 0.99"
+          "winOut, 0.20, -0.15, 0, 1"
+          "liner, 1, 1, 1, 1"
+          "md3_standard, 0.12, 0, 0, 1"
+          "md3_decel, 0.05, 0.80, 0.10, 0.97"
+          "md3_accel, 0.20, 0, 0.80, 0.08"
+          "overshot, 0.05, 0.85, 0.07, 1.04"
+          "crazyshot, 0.1, 1.22, 0.68, 0.98"
+          "hyprnostretch, 0.05, 0.82, 0, 1"
+          "menu_decel, 0.05, 0.82, 0, 1"
+          "menu_accel, 0.20, 0, 0.82, 0.10"
+          "easeInOutCirc, 0.78, 0, 0.15, 1"
+          "easeOutCirc, 0, 0.48, 0.38, 1"
+          "easeOutExpo, 0.10, 0.94, 0.23, 0.98"
+          "softAcDecel, 0.20, 0.20, 0.15, 1"
+          "md2, 0.30, 0, 0.15, 1"
+
+          "OutBack, 0.28, 1.40, 0.58, 1"
+        ];
+
+        animation = [
+          "border, 1, 1.6, liner"
+          "borderangle, 1, 82, liner, once"
+          "windowsIn, 1, 3.2, winIn, slide"
+          "windowsOut, 1, 2.8, easeOutCirc"
+          "windowsMove, 1, 3.0, wind, slide"
+          "fade, 1, 1.8, md3_decel"
+          "layersIn, 1, 1.8, menu_decel, slide"
+          "layersOut, 1, 1.5, menu_accel"
+          "fadeLayersIn, 1, 1.6, menu_decel"
+          "fadeLayersOut, 1, 1.8, menu_accel"
+          "workspaces, 1, 4.0, menu_decel, slide"
+          "specialWorkspace, 1, 2.3, md3_decel, slidefadevert 15%"
+        ];
       };
-    };
 
-    # Animations
-    animations = {
-      enabled = true;
+      # Window rules
+      windowrulev2 = [
+        "opacity 0.80 0.80,class:^(com.mitchellh.$terminal)$"
+        "opacity 0.80 0.80,class:^(discord)$"
+        "opacity 0.80 0.80,class:^(Spotify)$"
+        "opacity 0.75 0.75,class:^(thunar)$"
+        "opacity 0.92 0.92,class:^(steam)$"
+        "opacity 0.92 0.92,class:^(obsidian)$"
+        "opacity 0.92 0.92,class:^(Code)$"
+        # ... add more later
 
-      bezier = [
-        "wind, 0.05, 0.85, 0.03, 0.97"
-        "winIn, 0.07, 0.88, 0.04, 0.99"
-        "winOut, 0.20, -0.15, 0, 1"
-        "liner, 1, 1, 1, 1"
-        "md3_standard, 0.12, 0, 0, 1"
-        "md3_decel, 0.05, 0.80, 0.10, 0.97"
-        "md3_accel, 0.20, 0, 0.80, 0.08"
-        "overshot, 0.05, 0.85, 0.07, 1.04"
-        "crazyshot, 0.1, 1.22, 0.68, 0.98"
-        "hyprnostretch, 0.05, 0.82, 0, 1"
-        "menu_decel, 0.05, 0.82, 0, 1"
-        "menu_accel, 0.20, 0, 0.82, 0.10"
-        "easeInOutCirc, 0.78, 0, 0.15, 1"
-        "easeOutCirc, 0, 0.48, 0.38, 1"
-        "easeOutExpo, 0.10, 0.94, 0.23, 0.98"
-        "softAcDecel, 0.20, 0.20, 0.15, 1"
-        "md2, 0.30, 0, 0.15, 1"
+        # Hyprland recommended rules
+        "suppressevent maximize, class:.*"
+        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
 
-        "OutBack, 0.28, 1.40, 0.58, 1"
+        # Steam gaming (performance) rules
+        "monitor 1, class:^steam_app_\\d+$"
+        "workspace 10,class:^steam_app_\\d+$"
       ];
 
-      animation = [
-        "border, 1, 1.6, liner"
-        "borderangle, 1, 82, liner, once"
-        "windowsIn, 1, 3.2, winIn, slide"
-        "windowsOut, 1, 2.8, easeOutCirc"
-        "windowsMove, 1, 3.0, wind, slide"
-        "fade, 1, 1.8, md3_decel"
-        "layersIn, 1, 1.8, menu_decel, slide"
-        "layersOut, 1, 1.5, menu_accel"
-        "fadeLayersIn, 1, 1.6, menu_decel"
-        "fadeLayersOut, 1, 1.8, menu_accel"
-        "workspaces, 1, 4.0, menu_decel, slide"
-        "specialWorkspace, 1, 2.3, md3_decel, slidefadevert 15%"
+      workspace = [
+        "10, border:false, rounding:false"
+        "special:terminal, on-created-empty:[float; size 960 540] $terminal, persistent:false"
+      ];
+
+      layerrule = "blur, logout_dialog";
+
+      # Layout configuration
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+      };
+
+      master = {
+        new_status = "master";
+      };
+
+      # Miscellaneous
+      misc = {
+        force_default_wallpaper = 0;
+        disable_hyprland_logo = true;
+      };
+
+      # Input
+      input = {
+        kb_layout = "us";
+
+        follow_mouse = 1;
+        sensitivity = 0.30;
+        accel_profile = "flat";
+
+        touchpad = {
+          natural_scroll = true;
+          scroll_factor = 0.15;
+        };
+      };
+
+      gestures = {
+        workspace_swipe = true;
+      };
+
+      device = [
+        {
+          # ... add later
+        }
+      ];
+
+      # Keybindings
+      "$mod" = "SUPER";
+
+      bind = [
+        # Applications
+        "$mod, T, exec, $terminal"
+        "$mod, F, exec, $browser"
+        "$mod, D, exec, discord"
+        "$mod, S, exec, spotify"
+        "$mod, G, exec, steam"
+        "$mod, E, exec, $file_manager"
+        "$mod, C, exec, $code_editor"
+
+        "Ctrl+Alt, W, exec, hyprpanel -q || hyprpanel"
+        "$mod+Alt, G, exec, ~/nixos/scripts/gamemode.sh"
+
+        # Commands
+        "$mod, Q, killactive,"
+        "$mod, Delete, exit,"
+        "$mod, W, togglefloating,"
+        "$mod, J, togglesplit," # dwindle
+
+        # Move focus with mainMod + arrow keys
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+
+        # Scroll through existing workspaces with $mod + scroll
+        "$mod, mouse_down, workspace, e+1"
+        "$mod, mouse_up, workspace, e-1"
+
+        # Handle workspaces 10 and special
+        "$mod, 0, workspace, 10"
+        "$mod SHIFT, 0, movetoworkspace, 10"
+        "$mod, code:36, togglespecialworkspace, terminal"
+      ]
+      ++ (
+        # Binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+        builtins.concatLists (
+          builtins.genList (
+            i:
+            let
+              ws = i + 1;
+            in
+            [
+              "$mod, code:1${toString i}, workspace, ${toString ws}"
+              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+          ) 9
+        )
+      );
+
+      bindm = [
+        "$mod, Z, movewindow"
+        "$mod, X, resizewindow"
+
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+
+      # Laptop multimedia keys for volume and LCD brightness
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl s 5%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl s 5%-"
+      ];
+
+      bindl = [
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPause, exec, playerctl play-pause"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
       ];
     };
-
-    # Window rules
-    windowrulev2 = [
-      "opacity 0.80 0.80,class:^(com.mitchellh.$terminal)$"
-      "opacity 0.80 0.80,class:^(discord)$"
-      "opacity 0.80 0.80,class:^(Spotify)$"
-      "opacity 0.75 0.75,class:^(thunar)$"
-      "opacity 0.92 0.92,class:^(steam)$"
-      "opacity 0.92 0.92,class:^(obsidian)$"
-      "opacity 0.92 0.92,class:^(Code)$"
-      # ... add more later
-
-      # Hyprland recommended rules
-      "suppressevent maximize, class:.*"
-      "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-
-      # Steam gaming (performance) rules
-      "monitor 1, class:^steam_app_\\d+$"
-      "workspace 10,class:^steam_app_\\d+$"
-    ];
-
-    workspace = [
-      "10, border:false, rounding:false"
-      "special:terminal, on-created-empty:[float; size 960 540] $terminal, persistent:false"
-    ];
-
-    layerrule = "blur, logout_dialog";
-
-    # Layout configuration
-    dwindle = {
-      pseudotile = true;
-      preserve_split = true;
-    };
-
-    master = {
-      new_status = "master";
-    };
-
-    # Miscellaneous
-    misc = {
-      force_default_wallpaper = 0;
-      disable_hyprland_logo = true;
-    };
-
-    # Input
-    input = {
-      kb_layout = "us";
-
-      follow_mouse = 1;
-      sensitivity = 0.30;
-      accel_profile = "flat";
-
-      touchpad = {
-        natural_scroll = true;
-        scroll_factor = 0.15;
-      };
-    };
-
-    gestures = {
-      workspace_swipe = true;
-    };
-
-    device = [
-      {
-        # ... add later
-      }
-    ];
-
-    # Keybindings
-    "$mod" = "SUPER";
-
-    bind = [
-      # Applications
-      "$mod, T, exec, $terminal"
-      "$mod, F, exec, $browser"
-      "$mod, D, exec, discord"
-      "$mod, S, exec, spotify"
-      "$mod, G, exec, steam"
-      "$mod, E, exec, $file_manager"
-      "$mod, C, exec, $code_editor"
-
-      "Ctrl+Alt, W, exec, hyprpanel -q || hyprpanel"
-      "$mod+Alt, G, exec, ~/nixos/scripts/gamemode.sh"
-
-      # Commands
-      "$mod, Q, killactive,"
-      "$mod, Delete, exit,"
-      "$mod, W, togglefloating,"
-      "$mod, J, togglesplit," # dwindle
-
-      # Move focus with mainMod + arrow keys
-      "$mod, left, movefocus, l"
-      "$mod, right, movefocus, r"
-      "$mod, up, movefocus, u"
-      "$mod, down, movefocus, d"
-
-      # Scroll through existing workspaces with $mod + scroll
-      "$mod, mouse_down, workspace, e+1"
-      "$mod, mouse_up, workspace, e-1"
-
-      # Handle workspaces 10 and special
-      "$mod, 0, workspace, 10"
-      "$mod SHIFT, 0, movetoworkspace, 10"
-      "$mod, code:36, togglespecialworkspace, terminal"
-    ]
-    ++ (
-      # Binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-      builtins.concatLists (
-        builtins.genList (
-          i:
-          let
-            ws = i + 1;
-          in
-          [
-            "$mod, code:1${toString i}, workspace, ${toString ws}"
-            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-          ]
-        ) 9
-      )
-    );
-
-    bindm = [
-      "$mod, Z, movewindow"
-      "$mod, X, resizewindow"
-
-      "$mod, mouse:272, movewindow"
-      "$mod, mouse:273, resizewindow"
-    ];
-
-    # Laptop multimedia keys for volume and LCD brightness
-    bindel = [
-      ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-      ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-      ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-      ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-      ",XF86MonBrightnessUp, exec, brightnessctl s 5%+"
-      ",XF86MonBrightnessDown, exec, brightnessctl s 5%-"
-    ];
-
-    bindl = [
-      ", XF86AudioNext, exec, playerctl next"
-      ", XF86AudioPause, exec, playerctl play-pause"
-      ", XF86AudioPlay, exec, playerctl play-pause"
-      ", XF86AudioPrev, exec, playerctl previous"
-    ];
   };
 }
