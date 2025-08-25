9f5dd0b4235968720ab1ba8303c4199f7c43bfb0
Shonhh <endinja.versitile@gmail.com>
2025-08-24 23:36:24 -0500

    fixed scaling for spectre and fixed vscode keybind

--- hosts/nixospectre/home.nix
+++ hosts/nixospectre/home.nix
@@ -33,6 +33,7 @@
   ];
 
   home.shell.enableFishIntegration = true;
+  eDP-1.enable = true;
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
