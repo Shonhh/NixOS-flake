acfbdf88713f5a744255d24e21373680986a1d19
Shonhh <endinja.versitile@gmail.com>
2025-10-01 22:32:07 -0500

    new wallpapers?

--- nixos-modules/services/stylix.nix
+++ nixos-modules/services/stylix.nix
@@ -2,7 +2,7 @@
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    image = ../../home-modules/hypr/wallpaper.png;
+    image = ../../home-modules/hypr/wallpaper.jpg;
 
     targets = {
       spicetify.enable = false;
