4647ce0191282ad38861b40a170c404f57bdbc9a
Shonhh <endinja.versitile@gmail.com>
2025-10-02 00:56:10 -0500

    fixed wallpaper

--- scripts/rotate-eDP1.sh
+++ scripts/rotate-eDP1.sh
@@ -7,3 +7,6 @@ else
     hyprctl keyword monitor eDP-1,preferred,auto,2,transform,0
     hyprctl keyword input:touchdevice:transform 0
 fi
+
+# Re-apply the wallpaper to force a screen refresh
+hyprctl hyprpaper reload ,"~/nixos/home-modules/hypr/wallpaper.jpg"
