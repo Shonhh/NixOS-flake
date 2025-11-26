bd1440b918e2a8a8462fa7eb55a97888a6ec042c
Shonhh <endinja.versitile@gmail.com>
2025-11-26 12:46:17 -0600

    changing theme to gruvbox

--- home-modules/desktop/stylix.nix
+++ home-modules/desktop/stylix.nix
@@ -7,34 +7,6 @@
     hyprlock.enable = false;
   };
 
-  stylix.cursor = {
-    name = "catppuccin-mocha-dark-cursors";
-    package = pkgs.catppuccin-cursors.mochaDark;
-    size = 23;
-  };
-
-  stylix.fonts = {
-    serif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Serif";
-    };
-
-    sansSerif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans";
-    };
-
-    monospace = {
-      package = pkgs.nerd-fonts.hack;
-      name = "Hack Nerd Font Mono";
-    };
-
-    emoji = {
-      package = pkgs.noto-fonts-color-emoji;
-      name = "Noto Color Emoji";
-    };
-  };
-
   gtk = {
     enable = true;
 
--- nixos-modules/services/stylix.nix
+++ nixos-modules/services/stylix.nix
@@ -1,12 +1,40 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    image = ../../home-modules/hypr/wallpaper.jpg;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+    image = ../../home-modules/hypr/wallpaper2.png;
+    polarity = "dark";
+
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.fira-code;
+        name = "Fira Code Nerd Font";
+      };
+
+      sansSerif = {
+        package = pkgs.lato;
+        name = "Lato";
+      };
+    };
+
+    icons = {
+      enable = true;
+      package = pkgs.papirus-icon-theme;
+
+      dark = "Papirus-Dark";
+      light = "Papirus-Light";
+    };
 
     targets = {
       spicetify.enable = false;
       grub.enable = false;
+    };
+
+    cursor = {
+      package = pkgs.phinger-cursors;
+      name = "phinger-cursors";
+      size = 24;
     };
   };
 }
