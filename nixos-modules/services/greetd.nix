3bf6c35da475dfe19b3b67398888c2cb371cc9e7
Shonhh <endinja.versitile@gmail.com>
2025-12-03 21:41:15 -0600

    changed back to tuigreet, will change to better graphical login manager later

--- nixos-modules/services/greetd.nix
+++ nixos-modules/services/greetd.nix
@@ -1,31 +1,13 @@
 { pkgs, ... }:
 
-let
-  swayConfig = pkgs.writeText "greetd-sway-config" ''
-    # `-l` activates layer-shell mode for gtkgreet.
-    # `swaymsg exit` will run after gtkgreet finishes, closing the temporary Sway session.
-    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
-
-    # Binds Super+Shift+e to a power menu in the greeter.
-    bindsym Mod4+shift+e exec swaynag \
-      -t warning \
-      -m 'What do you want to do?' \
-      -b 'Poweroff' 'systemctl poweroff' \
-      -b 'Reboot' 'systemctl reboot'
-  '';
-in
 {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
+        command = "${pkgs.tuigreet}/bin/tuigreet -t --user-menu -r --asterisks --cmd Hyprland";
 	user = "greeter";
       };
     };
   };
-
-  environment.etc."greetd/environments".text = ''
-    Hyprland
-  '';
 }
