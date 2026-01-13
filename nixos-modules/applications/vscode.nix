5ebb3ea20706880ce8e13053fce5c393d6363715
Shonhh <endinja.versitile@gmail.com>
2026-01-13 13:45:14 -0600

    added vscode extensions

--- nixos-modules/applications/vscode.nix
+++ nixos-modules/applications/vscode.nix
@@ -1,4 +1,18 @@
 { pkgs, ... }:
 {
-  programs.vscode.enable = true;
+  programs.vscode = {
+    enable = true;
+
+    extensions = with pkgs.vscode-extensions; [
+      ms-vscode.cpptools
+      jdinhlife.gruvbox
+
+      ms-vscode-remote.vscode-remote-extensionpack
+      ms-vscode-remote.remote-ssh
+      ms-vscode.remote-explorer
+      ms-vscode-remote.remote-wsl
+      ms-vscode-remote.remote-containers
+      ms-vscode.makefile-tools
+    ];
+  };
 }
